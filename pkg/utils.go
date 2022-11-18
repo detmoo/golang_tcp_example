f325cfe4243cd36fcc3c3fcd20e8b08d825cf7fc
Oliver Richardson <oliver@bow-data.com>
2022-11-18 18:16:51 +0000

    error handling channel

--- pkg/utils.go
+++ pkg/utils.go
@@ -27,7 +27,7 @@ func (m *ServerClosedError) Error() string {
 
 func DeferCloseListener(listener net.Listener, timeout time.Duration, closureChannel chan<- error, parent context.Context) error {
 
-    closer := func(reason string) error {
+    closer := func(reason string) ServerClosedError {
         defer listener.Close()
         err := &ServerClosedError{
             Reason: reason,
