12e203152172c66f331358c19459a4b489154c68
Oliver Richardson <oliver@bow-data.com>
2022-11-23 18:25:03 +0000

    sophisticated server test

--- pkg/requests.go
+++ pkg/requests.go
@@ -48,13 +48,13 @@ func HandleIncomingRequest(conn net.Conn) error {
     // receive
     defer conn.Close()
     buffer := make([]byte, 1024)
-    len, err := conn.Read(buffer)
+    size, err := conn.Read(buffer)
     if err != nil {
         log.Fatal(err)
         return err
     }
     receivedMsg := new(Message)
-    receivedMsg.parse(buffer[:len])
+    receivedMsg.parse(buffer[:size])
     log.Println("this is the received.Content:", receivedMsg.Content)
     log.Println("this is the received.Metadata:", receivedMsg.Metadata)
 
@@ -80,12 +80,12 @@ func MakeRequest(msg Message, conn net.Conn) (answer Message, err error) {
 
     // receive
     output := make([]byte, 1024)
-    if len, err := conn.Read(output); err != nil {
+    if size, err := conn.Read(output); err != nil {
         log.Fatal(err)
     }
 
     log.Println("this is the raw output:", string(output))
-    answer.parse(output[:len])
+    answer.parse(output[:size])
     return
 }
 
